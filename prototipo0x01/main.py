026f4adf81859f6187fad7a156bc7303bc72d3b9
William <59394187+william64644@users.noreply.github.com>
2024-09-30 10:44:11 -0300

    refatorar o experimento 0x01 em forma do protótipo 0x01

---
+++ prototipo0x01/main.py
@@ -0,0 +1,55 @@
+"""
+Algorítimo baseado em um simples XOR entre a chave
+e a informação de entrada dividida em blocos de 8 bits
+
+Defeitos:
+Se uma informação criptografada for novamente criptografada novamente com a mesma chave, o algorítimo retorna a informação original
+Vulnerável a ataque por análise estatística
+Vulnerável a ataque por "força bruta" (brute force)
+"""
+def xor(bit1, bit2):
+    if (bit1 != bit2):
+        return '1'
+    else:
+        return '0'
+
+def XOR(byte1, byte2):
+    return ''.join(xor(byte1[i], byte2[i])for i in range(len(byte1)))
+
+def intBinary(integer):
+    return bin(integer)[2:]
+
+def binaryInt(bin):
+    return int(bin, 2)
+
+def strBinArr(string):
+    return (' '.join('{0:08b}'.format(ord(x), 'b') for x in string)).split()
+    
+def binArrStr(binArr):
+    out = ""
+    for bin in binArr:
+        out += chr(binaryInt(bin))
+    return out
+    
+def calcularChave(stringKey):
+    binary = strBinArr(stringKey)
+    out = binary[0]
+    for i in range(len(binary)-1):
+        out = XOR(out, binary[i+1])
+
+    return out
+
+def criptografar(entrada, chave):
+    entradaBin = strBinArr(entrada)
+    chave = calcularChave(chave)
+    criptografado = []
+    for byte in entradaBin:
+        criptografado.append(chr(binaryInt(XOR(byte, chave))))
+    return criptografado
+
+descriptografar = criptografar
+
+data = "abc123"
+key = "1"
+print(criptografar(data, key))
+print(descriptografar(criptografar(data, key), key))
